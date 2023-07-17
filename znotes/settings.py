6980c394c216266895fa6423f74534f7542c33e4
regmi2 <regmipallav@gmail.com>
2023-07-17 15:02:41 -0500

    vercel ready potentially

--- znotes/settings.py
+++ znotes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-pkxj0p0-wyb8z4a$xj01_#sd64f-5y#jaii-px1bx#6(61jynf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', 'localhost',]
 
 
 # Application definition
